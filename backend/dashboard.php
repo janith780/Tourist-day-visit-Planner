0fff8c6c838b96485abefeee36de1d22486b4e75
Janith <janith200207@gmail.com>
2026-04-05 00:03:44 +0530

    Create new dashboard_view.html file and modified two files

--- backend/dashboard.php
+++ backend/dashboard.php
@@ -1,12 +1,16 @@
 <?php
 session_start();
+
+// If user not logged in → redirect to login
 if (!isset($_SESSION['user'])){
     header("Location: frontend/login.html");
     exit();
 }
-//pass to html
+
+// Pass session variables to frontend
 $username = $_SESSION['user'];
 $role = isset($_SESSION['role']) ? $_SESSION['role'] : 'user';
 
+// Include dashboard view
 include("dashboard_view.php");
 ?>
