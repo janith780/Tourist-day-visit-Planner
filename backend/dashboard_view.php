8a09e2132f5f66cb9ae653cf7c1231b754a18d64
Janith <janith200207@gmail.com>
2026-04-04 22:56:49 +0530

    Modified two files

--- backend/dashboard_view.php
+++ backend/dashboard_view.php
@@ -1,6 +1,11 @@
 <?php if (isset($_GET['msg'])) { ?>
-    <div style="background: lightgreen; padding: 10px;">
-        Location added successfully!
+    <div style="background: lightgreen; padding: 10px; margin-bottom:10px;">
+        <?php
+        if($_GET['msg'] == 'added') echo "Location added successfully";
+        if($_GET['msg'] == 'deleted') echo "Location deleted successfully!";
+        if($_GET['msg'] == 'updated') echo "Location updated successfully!";
+        ?>
+
     </div>
 <?php } ?>
 
@@ -9,6 +14,20 @@
 <?php if ($role == 'admin') { ?>
     <h3>Admin Panel</h3>
 
+    <!-- TABS -->
+    <button onclick="showTab('add')">Add location</button>
+    <button onclick="showTab('update')">Update location</button>
+    <button onclick="showTab('delete')">Delete location</button>
+<hr>
+
+<?php
+include("db.php");
+$result = mysqli_query($conn, "SELECT * FROM location");
+?>
+
+  <!--add_location-->
+<div id="add" style="display:none;">
+    <h3>Add Location</h3>
     <form action="add_location.php" method="POST">
         Name: <input type="text" name="name"><br>
         Description: <input type="text" name="description"><br>
@@ -21,6 +40,104 @@
         Longitude: <input type="text" name="longitude"><br>
         <button type="submit">Add Location</button>
     </form>
+</div>
+
+<!-- UPDATE LOCATION -->
+<div id="update" style="display:none;">
+    <h3>Update Location</h3>
+
+    <table border="1" cellpadding="10">
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>District</th>
+            <th>Category</th>
+            <th>Open Time</th>
+            <th>Close Time</th>
+            <th>Ticket Price</th>
+            <th> Latitude</th>
+            <th>Longitude</th>
+            <th>Action</th>
+        </tr>
+
+       <?php
+       $result = mysqli_query($conn, "SELECT * FROM location");
+       while($row = mysqli_fetch_assoc($result)){?>
+        <tr>
+            <form action="update_location.php" method="POST">
+                <td>
+                    <input type="text" name="name" value="<?php echo $row['name']; ?>">required
+                </td>
+                <td>
+                    <input type="text" name="description" value="<?php echo $row['description']; ?>">
+                </td>
+                <td>
+                    <input type="text" name="district" value="<?php echo $row['district']; ?>">
+                </td>
+                <td>
+                    <input type="text" name="category" value="<?php echo $row['category']; ?>">
+                </td>
+                <td>
+                    <input type="time" name="open_time" value="<?php echo $row['open_time']; ?>">
+                </td>
+                <td>
+                    <input type="time" name="close_time" value="<?php echo $row['close_time']; ?>">
+                </td>
+                <td>
+                    <input type="number" name="ticket_price" value="<?php echo $row['ticket_price']; ?>">
+                </td>
+                <td>
+                    <input type="text" name="latitude" value="<?php echo $row['latitude']; ?>">
+                </td>
+                <td>
+                    <input type="text" name="longitude" value="<?php echo $row['longitude']; ?>">
+                </td>
+                <td>
+                    <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                    <button type="submit">Update</button>
+                </td>
+            </form>
+        </tr>
+        <?php } ?>
+    </table>
+</div>
+
+<!-- DELETE LOCATION -->
+<div id="delete" style="display:none;">
+    <h3>Delete Location</h3>
+
+    <?php
+    $result2 = mysqli_query($conn, "SELECT * FROM location");
+    ?>
+
+    <table border="1" cellpadding="10">
+        <tr>
+            <th>Name</th>
+            <th>Action</th>
+        </tr>
+
+        <?php while($row = mysqli_fetch_assoc($result2)) { ?>
+        <tr>
+            <td><?php echo $row['name']; ?></td>
+            <td>
+                <a href="delete_location.php?id=<?php echo $row['id']; ?>" 
+                onclick="return confirm('Are you sure?')">Delete</a>
+            </td>
+        </tr>
+        <?php } ?>
+    </table>
+</div>
+
+<!-- JAVASCRIPT -->
+<script>
+function showTab(tab) {
+    document.getElementById('add').style.display = 'none';
+    document.getElementById('update').style.display = 'none';
+    document.getElementById('delete').style.display = 'none';
+
+    document.getElementById(tab).style.display = 'block';
+}
+</script>
 
 <?php } else { ?>
     <p>User Dashboard</p>
--- backend/update_location.php
+++ backend/update_location.php
@@ -11,9 +11,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $id = $_POST['id'];
     $name = $_POST['name'];
     $description = $_POST['description'];
+    $district = $_POST['district'];
+    $category = $_POST['category'];
+    $open_time  = $_POST['open_time'];
+    $close_time = $_POST['close_time'];
+    $ticket_price = $_POST['ticket_price'];
+    $latitude = $_POST['latitude'];
+    $longitude = $_POST['longitude'];
 
-    $sql = "UPDATE location
-    SET name = '$name', description = '$description'
+if(!is_numeric($ticket_price)|| !is_numeric($latitude) || !is_numeric($longitude)){
+   echo "Price, Latitude and Longitude must be numbers!";
+   exit();
+}   
+
+
+    $sql = "UPDATE location SET 
+    name = '$name', 
+    description = '$description',
+    district = '$district',
+    category = '$category',
+    open_time = '$open_time',
+    close_time = '$close_time',
+    ticket_price = '$ticket_price',
+    latitude = '$latitude',
+    longitude = '$longitude'
     WHERE id = '$id'";
 
     if (mysqli_query($conn, $sql)){
